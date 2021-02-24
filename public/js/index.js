7b5b4f3fe46d6ed51405745e6474e9d3f8b2306d
Braian López <braian.lopez@outlook.com>
2021-02-24 14:35:42 -0300

    Done

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,23 @@ const elementsPos = document.querySelectorAll("span.pos");
 
 const cards = document.getElementsByClassName("what-said__item");
 
+// MENU
+const buttonNav = document.getElementById("button-nav");
+const nav = document.getElementById("nav");
+const elementChecket = document.getElementById("nav");
+const navCheck = document.getElementById("nav-check");
+const wrap = document.getElementById("wrap");
+
+function handleMenuAndSetBackground() {
+  navCheck.click();
+  nav.classList.toggle("display");
+  buttonNav.classList.toggle("icon-close");
+}
+
+buttonNav.addEventListener("click", handleMenuAndSetBackground);
+
+//SLIDER
+
 async function wait() {
   return new Promise((resolve) => {
     setTimeout(resolve, 900);
@@ -35,13 +52,13 @@ async function launcher(c) {
 function changeSelectPos() {
   for (let i = 0; i < elementsPos.length; i++) {
     if (elementsPos[i].classList.contains("select-pos")) {
-      elementsPos[i].classList.remove('select-pos')
-      if ((i + 1) < elementsPos.length) {
-        elementsPos[i+1].classList.add('select-pos')
-      }else {
-        elementsPos[0].classList.add('select-pos')
+      elementsPos[i].classList.remove("select-pos");
+      if (i + 1 < elementsPos.length) {
+        elementsPos[i + 1].classList.add("select-pos");
+      } else {
+        elementsPos[0].classList.add("select-pos");
       }
-      return 
+      return;
     }
   }
 }
